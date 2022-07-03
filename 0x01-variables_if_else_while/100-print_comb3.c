4e821f8ce568948cd4b4fd520402b79f72f2b2d2
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-03 06:33:11 -0700

    first full trial

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * program that prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i,j,k;
+
+	1 = 0
+
+		while (1 < 100)
+		{
+			j = i % 10
+				k = i / 10
+
+				if (k < j)
+				{
+					putchar (k + '0');
+					putchar (j + '0');
+
+					if (i < 89)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			i++;
+		}
+	putchar('\n');
+
+	return(0);
+}
