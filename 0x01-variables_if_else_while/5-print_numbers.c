3c42e8db2e1248c982031e390c4fa9f8eb4c8ca3
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-03 03:54:50 -0700

    print numbers 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * print all base 10 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
