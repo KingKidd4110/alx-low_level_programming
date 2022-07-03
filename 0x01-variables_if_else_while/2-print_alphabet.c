e7e10660a7482134d0eaa4e415b3b8d183ceb935
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-03 02:51:20 -0700

    print lower5 case 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * maim - maim
+ * program that prints the alphabet in lowercase, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char c='a';
+
+	while (z<='z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
