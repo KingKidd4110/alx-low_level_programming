f2f4751abee1e94f50ed64c0065337750a3d725c
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-04 05:03:20 -0700

    betty fix for 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - main
- * program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
- * return: 0
+ * prints all single digit numbers of base 10 starting from 0
+ * followed by a new line.
+ * Return: 0
  */
 int main(void)
 {
