339c8b76aa1aa8156fa21383c28c70b78cb8c1b5
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-04 04:14:12 -0700

    betty fix 1 last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - main
- * This program will assign a random number to the variable n each time it is executed
+ * Assign a random number to the variable n each time it is executed
  * Return: 0
  */
 int main(void)
