7f0efcbc578f618f8df72c4e29fa7d53298880c3
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-04 04:23:42 -0700

    betty fix print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * maim - maim
- * program that prints the alphabet in lowercase, followed by a new line.
+ * main - main
+ * Prints the alphabet in lowercase, followed by a new line.
  * Return: 0
  */
 int main(void)
 {
-	char c='a';
+	char c = 'a';
 
-	while (c<='z')
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
 	putchar('\n');
+
 	return (0);
 }
