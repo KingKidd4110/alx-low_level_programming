5e175bf1be6464c8915caf415c72d0dbb3f3cc66
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-04 05:34:53 -0700

    betty fix for task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int i, j, k;
 
-	i = 0
+	i = 0;
 
 		while (i < 100)
 		{
-			j = i % 10
-				k = i / 10
+			j = i % 10;
+				k = i / 10;
 
 				if (k < j)
 				{
@@ -31,4 +31,5 @@ int main(void)
 	putchar('\n');
 
 	return (0);
+
 }
