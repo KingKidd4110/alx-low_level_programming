22022a2680d55f1b790f9ff4bb85edb2f43895a5
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-05 13:34:07 -0700

    task 0 retry

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	char _putchar[] = "_putchar";
-	int i = 0;
-
-	while (_putchar[i] != '\0')
-	{
-		_putchar(_putchar[i]);
-		i++;
-	}
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
-
 	return (0);
 }
