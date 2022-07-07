2cd015e02f806a60642c0d22498443189a39f20b
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-07 07:19:35 -0700

    isupper-0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-fndef MAIN_H
+#ifndef MAIN_H
 
 #define MAIN_H
 
@@ -6,53 +6,27 @@ fndef MAIN_H
 
 int _putchar(char c);
 
+int _isupper(int c);
 
+int _isdigit(int c);
 
-void print_alphabet(void);
+int mul(int a, int b);
 
+void print_numbers(void);
 
+void print_most_numbers(void);
 
-void print_alphabet_x10(void);
+void more_numbers(void);
 
+void print_line(int n);
 
+void print_diagonal(int n);
 
-int _islower(int c);
+void print_square(int size);
 
+void print_triangle(int size);
 
-
-int _isalpha(int c);
-
-
-
-int print_sign(int n);
-
-
-
-int _abs(int);
-
-
-
-int print_last_digit(int);
-
-
-
-void jack_bauer(void);
-
-
-
-void times_table(void);
-
-
-
-int add(int, int);
-
-
-
-void print_to_98(int n);
-
-
-
-void print_times_table(int n);
+void print_number(int n);
 
 
 
