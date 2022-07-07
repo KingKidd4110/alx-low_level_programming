ef543ef4b6d86ac54206b8fd06ac1488cd6be732
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-07 09:34:22 -0700

    update typo in 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - prints 0 to 9 with new line
- * onluse _putchar
+ * only use _putchar
  * Description: use _putchar twice and print values 0-9
  */
 void print_numbers(void)
 {
 	int i;
 
-	i = o;
+	i = 0;
 	while (i < 10)
 	{
 		_putchar(i + '0');
