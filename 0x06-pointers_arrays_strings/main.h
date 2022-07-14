409da0991a2d17e12c1446849f96476173a1e3b3
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-14 09:57:03 -0700

    try this

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 
-
-
 #define MAIN_H
+
 
 int _putchar(char c);
 
@@ -25,5 +24,13 @@ char *leet(char *);
 char *rot13(char *);
 
 void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+int main(void);
+
+
 
 #endif /* MAIN_H */
