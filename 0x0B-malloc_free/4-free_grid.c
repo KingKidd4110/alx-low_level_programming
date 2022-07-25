a5ee675c75ddf45ca585e69a371f4cddc44fcbb6
Ayienda Brian <ayiendabrian@gmail.com>
2022-07-25 12:10:13 -0700

    mandatory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#unclude "main.h"
+/**
+ * free_grid - frees two dimentional grid
+ * @grid: grid
+ * @height: grid height
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+	for (i = 0; i < height; ++i)
+		free(grid[i]);
+	free(grid);
+}
