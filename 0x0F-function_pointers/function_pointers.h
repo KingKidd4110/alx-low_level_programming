e900ba36ebc2813a10e38ec09448f787a427c732
Ayienda Brian <ayiendabrian@gmail.com>
2022-08-03 09:43:16 -0700

    itterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
@@ -8,7 +10,5 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
-
-#include <stddef.h>
 
 #endif
