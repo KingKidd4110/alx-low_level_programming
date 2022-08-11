c346f67c8132313d2187ea6dcea3984d6940ea98
Ayienda Brian <ayiendabrian@gmail.com>
2022-08-11 12:45:29 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a list
+ * @h: current node
+ *
+ * Return: number of nodes
+ */
+
+
+
+size_t print_list(const list_t *h)
+{
+	const list_t *cnode;
+	size_t c;
+
+	c = 0;
+	cnode = h;
+
+	if (cnode->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+
+	else
+	{
+		printf("[%d] %s\n", cnode->len, cnode->str);
+		cnode = cnode->next;
+		c++;
+
+	}
+
+	return (c);
+}
