6390b2b08699afa46da6ce6109bcb4dc8796ef04
Ayienda Brian <ayiendabrian@gmail.com>
2022-08-12 12:16:00 -0700

    .asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+	extern printf	;	call the printf function
+	
+	section .data	;	initialise data section
+
+msg db 'Hello, Holberton', 0xa  ;	message to be printed
+
+fmt:	    	db "%s", 10, 0	; format
+
+	section .text	;	code text
+	global main	;	code
+main			;	entry point label
+
+	push rbp	;	push value itno stack
+	mov rdi,fmt	;	format
+	mov rsi,msg	;	store values
+	mov rax,0	;	assign 64bit register
+
+	call printf	;	calls c printf function
+
+	pop rbp		;	restore stack from push instruction
+
+	ret	
+
