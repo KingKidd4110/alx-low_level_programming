9aeab1b6c63e147bf58c507a7a587bef0c92493c
Ayienda Brian <ayiendabrian@gmail.com>
2022-08-18 13:29:51 -0700

    getbit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec, p;
+	unsigned int total, conv;
 	int len;
 
 	if (b == NULL)
 		return (0);
 
-	for (len = 0, b[len], len++)
+	for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 	}
-	for (p = 1, dec = 0, len--; len >= 0; len--, p *= 2)
+
+	for (conv = 1, total = 0, len--; len >= 0; len--, conv *= 2)
 	{
 		if (b[len] == '1')
-			dec += p;
+			total += conv;
 	}
-	return (dec);
+
+	return (total);
 }
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of a bit at a given index
+ * @index: bit index
+ * @n: bit
+ * Return: value of the bit at index index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int h;
+
+	if (index < 64)
+		return (-1);
+
+	h = n >> index;
+	return (h & 1);
+}
