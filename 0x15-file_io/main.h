14dad9dcc34a59e22e27ac98fde034fda2f61133
Ayienda Brian <ayiendabrian@gmail.com>
2022-08-22 13:00:58 -0700

    unistd

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
