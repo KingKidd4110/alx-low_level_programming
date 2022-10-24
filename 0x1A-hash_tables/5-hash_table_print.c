cd0e3c517c376e005130fa2dc8b4b9351aca8139
KingKidd4110 <ayiendabrian@gmail.com>
2022-10-23 17:53:40 -0700

    betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,10 +12,10 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *tmp;
 	char fncheck = 0;
 
-	if (ht == NULL)
+	if (ht == NULL; ht->array == NULL)
 		return;
 
-	print("{");
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
