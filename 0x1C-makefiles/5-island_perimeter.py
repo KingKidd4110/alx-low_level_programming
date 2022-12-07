4cfe8272413fdb43fc38860d220f8677a0bc09a5
KingKidd4110 <ayiendabrian@gmail.com>
2022-12-07 15:11:50 -0800

    island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+*** Defines class island_perimeter ***
+
+
+def island_perimeter(grid):
+    *** variables ***
+
+    x = len(grid[0])
+
+    y = len(grid)
+
+    perimeter = 0
+
+    
+
+    for i in range(y):
+
+        for j in range(x):
+
+            
+
+            if grid[i][j] == 1:
+
+                if j == 0 or grid[i][j-1] == 0:
+
+                    perimeter += 1
+
+                if j == x-1 or grid[i][j+1] == 0:
+
+                    perimeter += 1
+
+                if i == 0 or grid[i-1][j] == 0:
+
+                    perimeter += 1
+
+                if i == y-1 or grid[i+1][j] == 0:
+
+                    perimeter += 1
+
+                    
+
+    return perimeter
